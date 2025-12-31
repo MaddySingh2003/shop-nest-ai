199248faedeef1de6e03c89326001bc72d752bfb
unknown <MilanSuryavanshi27@gmail.com>
2025-12-31 15:20:40 +0530

    restart

--- ecom-frontend/src/main.ts
+++ ecom-frontend/src/main.ts
@@ -1,15 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-
+import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { routes } from './app/app.routes';
-import { jwtInterceptor } from './app/core/interceptors/jws.intercepter';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
-    provideHttpClient(withFetch())
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
