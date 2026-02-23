ffebbabe2a35ed47041b19a758d1e0c40ff32b69
MaddySingh2003 <Milansuryavanshi27@gmail.com>
2026-02-23 12:43:54 +0530

    edit

--- ecom-frontend/src/app/pages/profile/profile.ts
+++ ecom-frontend/src/app/pages/profile/profile.ts
@@ -1,47 +1,102 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ProfileService } from "../../services/profile.service";
 import { FormsModule } from "@angular/forms";
-import { Navbar } from "../../components/navbar/navbar";
-import { RouterModule } from "@angular/router";
+import { AddressService } from "../../services/adress.service";
 
 @Component({
-    selector:'app-profile',
-    standalone:true,
-    imports:[CommonModule,FormsModule],
-    templateUrl:'./profile.html'
+  selector: 'app-profile',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './profile.html'
 })
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
 
-    user:any=null;
-    loading=true;
-    profile:any={};
-    edit:boolean=false;
+  user: any = null;
+  loading = true;
+  profile: any = {};
+  edit: boolean = false;
 
-    constructor(
-        private profileService:ProfileService,
-        private cdr:ChangeDetectorRef
-    ){}
+  addresses: any[] = [];
+  selectedAddressId: number | null = null;
 
-    ngOnInit() {
-        this.loadProfile();
-    }
+  showAddressForm = false;
 
-    loadProfile(){
-        this.profileService.getProfile().subscribe(res=>{
-            console.log("PROFILE",res);
-            this.user=res;
-            this.loading=false;
-            this.cdr.detectChanges();
-        })
-    }
-   updateProfile(){
-    this.profileService.updateProfile(this.profile).subscribe(res=>{
-      this.profile=res;
-      this.edit=false;
-      alert("Profile updated");
-      this.cdr.detectChanges();
+  newAddress: any = {
+    fullName: '',
+    phone: '',
+    street: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    country: 'India'
+  };
+
+  constructor(
+    private profileService: ProfileService,
+    private addressService: AddressService
+  ) {}
+
+  ngOnInit() {
+    this.loadProfile();
+    this.loadAddresses();
+  }
+
+  loadProfile() {
+    this.profileService.getProfile().subscribe(res => {
+      this.user = res;
+      this.profile = { ...res };
+      this.loading = false;
     });
   }
 
+  updateProfile() {
+    this.profileService.updateProfile(this.profile).subscribe(res => {
+      this.user = res;
+      this.profile = { ...res };
+      this.edit = false;
+      alert("Profile updated");
+    });
+  }
+
+  loadAddresses() {
+    this.addressService.getMyAddresses().subscribe((res: any) => {
+      this.addresses = res || [];
+      if (this.addresses.length > 0) {
+        this.selectedAddressId = this.addresses[0].id;
+      }
+    });
+  }
+
+  saveAddress() {
+    this.addressService.addAddress(this.newAddress).subscribe(() => {
+      this.showAddressForm = false;
+
+      // ✅ reset properly
+      this.newAddress = {
+        fullName: '',
+        phone: '',
+        street: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        country: 'India'
+      };
+
+      this.loadAddresses();
+    });
+  }
+
+  editAddress(a: any) {
+    this.newAddress = { ...a };
+    this.showAddressForm = true;
+  }
+
+  deleteAddress(id: number) {
+    if (!confirm("Delete this address?")) return;
+
+    this.addressService.deleteAddress(id).subscribe(() => {
+      this.loadAddresses();
+    });
+  }
 }
